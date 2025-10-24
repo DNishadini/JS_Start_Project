98ec5e386bd9c9e3b8afa074e8b0249589d72807
DNishadini <dinushikaherath150@gmail.com>
2025-10-24 22:58:01 +0530

    Variable

---
+++ JS/variable.js
@@ -0,0 +1,16 @@
+/*//automatic hoisting
+myName = "Dinushika"
+//variable hoisting
+var myCity = "Kurunegala"
+console.log(myName)
+console.log(myCity)
+alert(myName)*/
+
+var myName = "Dinushika";
+var myAge = 15;
+
+function getMyName(){
+    console.log(myName)
+}
+
+getMyName()
