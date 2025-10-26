56acaab748d31b13763a2de3d6677e027ebf29e9
DNishadini <dinushikaherath150@gmail.com>
2025-10-26 10:30:08 +0530

    update

---
+++ JS/function.js
@@ -0,0 +1,70 @@
+//TRADITIONAL FUNCTIONS
+/*function getName(){
+    console.log('Janitha')
+}
+
+function getTotal(){
+    const num1=12
+    const num2=15
+    console.log(num1+num2)
+}
+getName()
+getTotal()*/
+
+//ANNONYMOUS FUNCTION
+/*const name = function(){
+    return "JD"
+}
+console.log(name())*/
+
+
+/*//ARROW FUNCTIONS
+const getName = () => {
+    console.log('Good Day')
+}
+getName()
+
+//annonymous arrow function
+() => {
+   console.log('Good Day') 
+}*/
+
+//ARGUMENT & PARAMETER
+
+//paramter vriable
+function getName(name){
+    console.log(name)
+}
+
+//argument
+getName('Rikash')
+
+
+/*function getTotal(num1, num2){
+    console.log(num1 + num2);
+}
+
+getTotal(12,15)*/
+
+
+/*const getTotal = function (num1, num2){
+    console.log(num1 + num2);
+}
+
+getTotal(12,15)*/
+
+/*const getSumofTwoNum = (num1, num2) => {
+    console.log(num1+num2)
+}
+getSumofTwoNum(12,30)*/
+
+//TRADITIONAL & ARROW function using ONE PARAMETER
+function readname(name){
+    console.log(name)
+}
+getName('Kalpana')
+
+const showName = name =>{
+    console.log(name)
+}
+showName('Shaluka')
--- JS/variable.js
+++ JS/variable.js
@@ -6,11 +6,50 @@ console.log(myName)
 console.log(myCity)
 alert(myName)*/
 
-var myName = "Dinushika";
+/*var myName = "Dinushika";
 var myAge = 15;
 
 function getMyName(){
-    console.log(myName)
+    var myWeight = 80
+    //redeclaration
+    var myWeight = 50
+    console.log(myWeight)
+}
+
+
+getMyName()*/
+
+
+let myWeight = 60
+//using let keyword we can't redeclare the variable
+let myWeight = 100
+console.log(myWeight)
+
+function getMyName(){
+
+}
+
+getMyName()
+
+let myWeight = 60
+//but changing the scope we can create same variable name using let
+
+
+function getMyName(){
+    let myWeight = 100
+    console.log(myWeight)
+}
+
+getMyName()
+
+//can't redecalared but can reassigned same scope
+let myWeight = 60
+//using let keyword we can't redeclare the variable
+myWeight = 100
+
+
+function getMyName(){
+    console.log(myWeight)
 }
 
 getMyName()
